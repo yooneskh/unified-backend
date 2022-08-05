b466b35a2ca49c7ef776753f865ab654fb75467f
Yoones Khoshghadam <yooneskh@gmail.com>
2022-08-05 22:12:29 +0430

    fix (authentication): change to sms

--- modules/authentication/lib/listeners.ts
+++ modules/authentication/lib/listeners.ts
@@ -1,47 +1,21 @@
 import { EventEmitter } from '../../../services/event-emitter.ts';
 import { IVerificationToken } from '../verification-tokens/interfaces.d.ts';
-import { makeSendInBlueEmail } from '../../../plugins/send-in-blue/send-in-blue-agent.ts';
 import { Config } from '../../../config.ts';
+import { sendLookupSMS } from '../../../plugins/kavenegar/kavenegar-agent.ts';
 
 
 EventEmitter.on('Resource.VerificationToken.Created', async (_event: string, _verificationTokenId: string, verificationToken: IVerificationToken) => {
 
   const { channel, channelIdentifier, code } = verificationToken;
 
-  if (channel !== 'email' || code === Config.authentication.staticVerificationCode) {
+  if (channel !== 'sms' || code === Config.authentication.staticVerificationCode) {
     return;
   }
 
-
-  const htmlContent = `
-    <html>
-      <body>
-        <h1>
-          Welcome To AboutShiraz
-        </h1>
-        <p>
-          You have reqeusted a verification code from AboutShiraz. If you have not, please ignore this email.
-        </p>
-        <p>
-          Verification Code: <strong>${code}</strong>
-        </p>
-      </body>
-    </html>
-  `;
-
-
-  await makeSendInBlueEmail({
-    ApiKey: Config.notifications.sendInBlue.apiKey,
-    senderName: 'AboutShiraz',
-    senderEmail: 'noreply@aboutshiraz.com',
-    receivers: [
-      {
-        name: 'Authentication',
-        email: channelIdentifier
-      }
-    ],
-    subject: 'Your Verification Code',
-    content: htmlContent
+  await sendLookupSMS({
+    receptor: channelIdentifier,
+    template: 'XXX',
+    token: code
   });
 
 });
