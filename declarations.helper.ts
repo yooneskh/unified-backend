21f10d5da496fbbb50c804c1f48810d2e53695ca
Yoones Khoshghadam <yooneskh@gmail.com>
2023-04-11 17:49:59 +0330

    chore (types): remove declarations helper

--- declarations.helper.ts
+++
@@ -1,8 +0,0 @@
-import './plugins/router-provider/addon.ts';
-import './plugins/router-rest-templates/addon.ts';
-import './plugins/rate-limiter/rate-limiter-router-addon.ts';
-import './plugins/svg-captcha/router-addon.ts';
-import './plugins/router-validators/addon.ts';
-import './plugins/setting-resource-maker/setting-resource-router.d.ts';
-import './modules/authentication/lib/router-addon.ts';
-import './modules/authorization/lib/router-addon.ts';
