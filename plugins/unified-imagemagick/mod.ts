d01f3e94ce93d0f7f45ee6c4f7b988168da57c46
Yoones Khoshghadam <yooneskh@gmail.com>
2023-04-28 04:45:37 +0330

    fix (image magick): rework changing format

--- plugins/unified-imagemagick/mod.ts
+++ plugins/unified-imagemagick/mod.ts
@@ -6,39 +6,45 @@ import {
   MagickGeometry,
 } from "https://deno.land/x/imagemagick_deno@0.0.19/mod.ts";
 
-
 let isImageMagickInitialized = false;
 
 
-function readImageMagick(filePath: string): Promise<IMagickImage> {
-  return new Promise((resolve, reject) => {
-    Deno.readFile(filePath)
-      .then(fileData => {
-        ImageMagick.read(fileData, resolve)
-      })
-      .catch(reject)
-  });
-}
+// function readImage(filePath: string) {
+//   return new Promise<IMagickImage>((resolve, reject) => {
+//     Deno.readFile(filePath)
+//       .then(fileData => {
 
-function writeImageMagick(image: IMagickImage, format: MagickFormat, filePath: string) {
-  return new Promise((resolve, reject) => {
-    image.write(
-      (data) => {
+//         ImageMagick.read(fileData, (image: IMagickImage) => {
 
-        if (!data) {
-          reject();
-        }
+//           if (!image) {
+//             return reject('could not read file data');
+//           }
 
-        Deno.writeFile(filePath, data)
-          .then(resolve)
-          .catch(reject);
+//           resolve(image);
 
-      },
-      format
-    );
-  })
-}
+//         });
 
+//       })
+//       .catch(reject)
+//   })
+// }
+
+// function writeImage(image: IMagickImage, filePath: string, format: MagickFormat) {
+//   return new Promise((resolve, reject) => {
+//     image.write((data: Uint8Array) => {
+
+//       if (!data) {
+//         return reject('could not get image data to write');
+//       }
+
+//       (Deno.writeFile(filePath, data)
+//         .then(resolve)
+//         .catch(reject)
+//       );
+
+//     }, format);
+//   });
+// }
 
 export interface IConvertWebp {
   inputFilePath: string;
@@ -50,15 +56,44 @@ export interface IConvertWebp {
 export async function transformToWebp({ inputFilePath, outputFilePath, width, height }: IConvertWebp) {
 
   if (!isImageMagickInitialized) {
-    await initializeImageMagick();
+  await initializeImageMagick();
     isImageMagickInitialized = true;
   }
 
+  // const image = await readImage(inputFilePath);
+  // image.resize(new MagickGeometry((!width ? '' : Math.min(width, image.width)) + 'x' + (!height ? '' : Math.min(height, image.height))))
+  // await writeImage(image, outputFilePath, MagickFormat.Webp);
 
-  const image = await readImageMagick(inputFilePath);
+  return new Promise((resolve, reject) => {
 
-  image.resize(new MagickGeometry((!width ? '' : Math.min(width, image.width)) + 'x' + (!height ? '' : Math.min(height, image.height))))
+    (Deno.readFile(inputFilePath)
+      .then(inputData => {
 
-  await writeImageMagick(image, MagickFormat.Webp, outputFilePath);
+        if (!inputData) {
+          return reject('could not read file');
+        }
+
+        ImageMagick.read(inputData, (image: IMagickImage) => {
+
+          if (!inputData) {
+            return reject('image magick could not read file');
+          }
+
+          image.resize(new MagickGeometry((!width ? '' : Math.min(width, image.width)) + 'x' + (!height ? '' : Math.min(height, image.height))))
+
+          image.write((outData: Uint8Array) => {
+            (Deno.writeFile(outputFilePath, outData)
+              .then(resolve)
+              .catch(reject)
+            );
+          }, MagickFormat.Webp);
+
+        });
+
+      })
+      .catch(reject)
+    );
+
+  });
 
 }
