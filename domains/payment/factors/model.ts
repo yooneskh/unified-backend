b482348bd732425203352551bad470ef6dc58e68
Yoones Khoshghadam <yooneskh@gmail.com>
2023-09-12 01:07:25 +0330

    fix (factors): remove titles and payed at label format

--- domains/payment/factors/model.ts
+++ domains/payment/factors/model.ts
@@ -5,35 +5,30 @@ FactorMaker.setProperties({
   name: {
     type: 'string',
     required: true,
-    title: 'نام',
-    titleable: true
+    titleable: true,
   },
   amount: {
     type: 'number',
     required: true,
-    title: 'مقدار'
   },
   user: {
     type: 'string',
     ref: 'User',
-    title: 'کاربر'
   },
   payed: {
     type: 'boolean',
-    title: 'پرداخت شده'
   },
   payedAt: {
     type: 'number',
-    title: 'زمان پرداخت'
+    labelFormat: 'YYYY/MM/DD HH:mm',
   },
   payticket: {
     type: 'string',
     ref: 'Payticket',
-    title: 'بلیط پرداخت'
   },
   meta: {
     type: 'object',
-    hidden: true
+    hidden: true,
   }
 });
 
